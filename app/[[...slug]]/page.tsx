d6bda703d747316a86f4913a99e0f9a6b6bb9be3
psinelnikov <pavel@sinelnikov.xyz>
2025-01-24 20:29:01 -0800

    Resolving remote build issue

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -10,6 +10,7 @@ import { notFound } from "next/navigation";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import Web3Provider from "@/components/providers/Web3Provider";
 import { ApolloProvider } from "@/components/providers/ApolloProvider";
+import type { MDXComponents } from "mdx/types";
 
 // Define which paths should include the Web3Provider
 const WEB3_ENABLED_PATHS = ["demo"];
@@ -17,6 +18,10 @@ const WEB3_ENABLED_PATHS = ["demo"];
 interface PageProps {
   params: Promise<{ slug?: string[] }>;
 }
+
+const mdxComponents: MDXComponents = {
+  ...defaultMdxComponents,
+};
 
 export default async function Page(props: PageProps) {
   const { slug = [] } = await props.params;
@@ -30,22 +35,22 @@ export default async function Page(props: PageProps) {
 
   const MDX = page.data.body;
 
-  const Content = (
-    <DocsBody>
-      <MDX components={{ ...defaultMdxComponents }} />
-    </DocsBody>
-  );
-
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       {shouldEnableWeb3 ? (
         <ApolloProvider>
-          <Web3Provider>{Content}</Web3Provider>
+          <Web3Provider>
+            <DocsBody>
+              <MDX components={mdxComponents} />
+            </DocsBody>
+          </Web3Provider>
         </ApolloProvider>
       ) : (
-        Content
+        <DocsBody>
+          <MDX components={mdxComponents} />
+        </DocsBody>
       )}
     </DocsPage>
   );
