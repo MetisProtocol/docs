1ac1b3abfc5e7b7c0a3c957d00a8fd36c46e3aca
psinelnikov <pavel@sinelnikov.xyz>
2025-02-12 16:01:23 -0700

    Fixed build errors

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Handle native binary modules
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "node-loader",
+    });
+
+    return config;
+  },
 };
 
 export default withMDX(config);
