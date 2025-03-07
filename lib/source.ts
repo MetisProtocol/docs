3ce6a68804e235cc9e9701708e73d92eda8eaa17
psinelnikov <pavel@sinelnikov.xyz>
2025-03-07 14:45:29 -0500

    Added icons and replaced link

--- lib/source.ts
+++ lib/source.ts
@@ -1,6 +1,8 @@
 import { docs, meta } from "@/.source";
 import { createMDXSource } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement } from "react";
 
 export const source = loader({
   baseUrl: "/",
@@ -10,5 +12,13 @@ export const source = loader({
       path: "demo",
       pattern: /^demo\//,
     },
+  },
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
   },
 });
