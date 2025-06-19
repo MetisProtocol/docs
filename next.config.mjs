fa5a788a9fab4a4edc2331c881eef701c4aa2d8b
Pavel Sinelnikov <pavel@sinelnikov.xyz>
2025-06-19 15:04:14 +0100

    Typos and color changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  // webpack: (config) => {
-  //   // Handle native binary modules
-  //   config.module.rules.push({
-  //     test: /\.node$/,
-  //     use: "node-loader",
-  //   });
-
-  //   return config;
-  // },
 };
 
 export default withMDX(config);
